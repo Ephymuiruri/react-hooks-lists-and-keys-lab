43cce49b81200904a7e283abfac0ab450b98f178
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-25 13:27:35 +0300

    using key for lists map methods

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies,id }) {
+  console.log({id})
+  const spanList = technologies.map((tech,index)=> 
+  (<span key={index}>{tech}</span>))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {spanList}
       </div>
     </div>
   );
